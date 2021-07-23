3d282271200e643cbe60979fa8eb2a69bf7c0074
Oliver Voros <olivervoros@gmail.com>
2021-07-23 22:43:47 +0200

    passport auth

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\Providers;
-
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+
+use Laravel\Passport\Passport;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,8 +15,9 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
+
 
     /**
      * Register any authentication / authorization services.
@@ -24,7 +27,6 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
+        Passport::routes();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\NoteController;
+use App\Http\Controllers\PassportAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,29 +16,35 @@ use App\Http\Controllers\NoteController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-# 1. Create and edit contacts
-Route::post('/contacts', [ContactController::class, 'create']);
-Route::put('/contacts/{contactId}', [ContactController::class, 'edit']);
 
-# 2. Retrieve a paginated list of all contacts
-Route::get('/contacts', [ContactController::class, 'all']);
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
 
-# 3. Retrieve a single contact
-Route::get('/contacts/{contactId}', [ContactController::class, 'get']);
+Route::middleware('auth:api')->group(function () {
 
-# 4. Can store multiple contacts for the same company
-Route::post('/multiple-contacts/{companyId}', [ContactController::class, 'storeMultipleContactsForACompany']);
+    # 1. Create and edit contacts
+    Route::post('/contacts', [ContactController::class, 'create']);
+    Route::put('/contacts/{contactId}', [ContactController::class, 'edit']);
 
-# 5. Store notes against a contact
-Route::post('/add-notes/{contactId}', [NoteController::class, 'addNotes']);
+    # 2. Retrieve a paginated list of all contacts
+    Route::get('/contacts', [ContactController::class, 'all']);
 
-# 6. List all contacts at a given company
-Route::get('/list-all-contacts-for-a-company/{companyId}', [ContactController::class, 'listByCompanyId']);
+    # 3. Retrieve a single contact
+    Route::get('/contacts/{contactId}', [ContactController::class, 'get']);
 
-# 7. List all companies
-Route::get('/companies', [CompanyController::class, 'all']);
+    # 4. Can store multiple contacts for the same company
+    Route::post('/multiple-contacts/{companyId}', [ContactController::class, 'storeMultipleContactsForACompany']);
 
-# 8. Search for contacts by name or company
-Route::get('/search-for-contact-by-company/{companyName}', [ContactController::class, 'searchByCompanyName']);
-Route::get('/search-for-contact-by-name/{contactName}', [ContactController::class, 'searchByContactName']);
+    # 5. Store notes against a contact
+    Route::post('/add-notes/{contactId}', [NoteController::class, 'addNotes']);
 
+    # 6. List all contacts at a given company
+    Route::get('/list-all-contacts-for-a-company/{companyId}', [ContactController::class, 'listByCompanyId']);
+
+    # 7. List all companies
+    Route::get('/companies', [CompanyController::class, 'all']);
+
+    # 8. Search for contacts by name or company
+    Route::get('/search-for-contact-by-company/{companyName}', [ContactController::class, 'searchByCompanyName']);
+    Route::get('/search-for-contact-by-name/{contactName}', [ContactController::class, 'searchByContactName']);
+});
